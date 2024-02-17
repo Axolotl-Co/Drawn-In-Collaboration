b7cd99430aba140790dcd87b85665f88a38ef7a6
Kevin Can <kevincan321@gmail.com>
2024-02-16 20:58:59 -0800

    Dev merge to production main (#5)
    * webpack config and package json created
    
    * updated build 2 (#2)
    
    * Delete node_modules directory
    
    * Delete package.json
    
    * Delete package-lock.json
    
    * Delete webpack.config.js
    
    * Delete .gitignore
    
    * updated fixed dev

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../dist')));
+
+//API routes
+// app.get()
+// app.post()
+// app.put()
+// app.delete()
+  
+     
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../dist/index.html'));
+// });
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
