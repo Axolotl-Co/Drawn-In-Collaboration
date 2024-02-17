95c323f104488b1841514d9adff930c6cb80ea1b
Kelementz916 <kevincan321@gmail.com>
2024-02-16 20:30:05 -0800

    fix for current files

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 // app.put()
 // app.delete()
 
-   
+     
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 // app.get('*', (req, res) => {
