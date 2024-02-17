081eaf09b537bd2bb4a04bb40fa6b70b5abe2bc9
Allison Scott <156137869+AllisonScott97@users.noreply.github.com>
2024-02-17 13:06:34 -0600

    added redux, App component, HTML Canvas (#6)
    Co-authored-by: Allison Scott <allisonscott@Allisons-MacBook-Pro.local>
    Co-authored by: Matt Konop <mattsterprogrammer@gmail.com>
    Co-authored by: Kevin Can <kevincan321@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: process.env.NODE_ENV,
     entry: {
-        bundle: path.resolve(__dirname, 'client/main.js') //can have multiple entry points for code splitting
+        bundle: path.resolve(__dirname, 'client/main.jsx') //can have multiple entry points for code splitting
     },
     output: {
         filename: '[name].js',
@@ -43,7 +43,7 @@ module.exports = {
         }),
     ],
     devServer: {
-      contentBase: path.join(__dirname, 'dist'),
+    //   contentBase: path.join(__dirname, 'dist'), //received webpack-CLI error with this so commenting out for now
       compress: true, //makes a g zip compression- aka quicker
       port: 8080,
       open: true,
