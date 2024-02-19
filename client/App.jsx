84ea2dcbab7413c0987313039984f7125649528f
M Konop <155488177+MattKonop@users.noreply.github.com>
2024-02-18 18:48:22 -0800

    Got Basic Socket.io working, Chaged Server Rendering to include Web Socket (#13)
    Co-authored-by: KevinCan <kevincan321@gmail.com>
    Co-authored-by: AllisonScott <allisonbscott97@gmail.com>

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,27 @@
 
-import React, { useState, useLayoutEffect, useRef } from "react";
+import React, { useState, useLayoutEffect, useRef, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom"; //assuming we'll be using react router but can change if needed
 import rough from "roughjs";
 import Canvas from './components/Canvas.jsx';
 import Signup from './components/Signup.jsx';
 import Login from "./components/Login.jsx";
+import { io } from "socket.io-client"
+const socket = io.connect('http://localhost:3000') // server ... we can also put this in a serparate component and import it 
+
+
+
 
 const App = () => {
+  // Event listener for connection
+  const sendMessge = () => {
+
+    socket.emit('send_message', 10, 'HI', {a : 'ehhhh'});
+  }
+    // Cleanup function to disconnect the socket when the component unmounts
+ 
+
+
+
   //sets state of elements on canvas to empty array
   const [elements, setElements] = useState([]);
   //sets state of drawing by user to false
@@ -16,6 +31,10 @@ const App = () => {
   //render the HTML canvas element
   return (
     <div>
+
+
+    <button onClick={sendMessge}> Send Message</button>
+
       <Signup>Signup</Signup>
       <Login>Login</Login>
       <Canvas
