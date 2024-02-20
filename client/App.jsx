a85a073d9444bb33c4a896207597594ba35ebaae
Kevin Can <kevincan321@gmail.com>
2024-02-20 12:36:54 -0800

    Kc2 (#19)
    * move to feature branch
    
    * updated login and root routing
    
    Co-authored-by: MattKonop <mattsterprogrammer@gmail.com>
    Co-authored-by: AllisonScott <allisonbscott97@gmail.com>”
    
    ---------
    
    Co-authored-by: MattKonop <mattsterprogrammer@gmail.com>

--- client/App.jsx
+++ client/App.jsx
@@ -37,6 +37,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/canvas" element={<Canvas
           elements={elements}
